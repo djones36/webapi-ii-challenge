110764d7fc5d1bb1dd65f2574d2f6b96a9555227
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-02 09:41:35 -0700

    get by id/comments

--- api/postEndpoint.js
+++ api/postEndpoint.js
@@ -26,16 +26,34 @@ router.get("/:id", (req, res) => {
     .then(post => {
       post && post.length
         ? res.json(post)
-        : res
-            .status(404)
-            .json({
-              message: "The post with the specified ID does not exist."
-            });
+        : res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
     })
     .catch(err => {
       res
         .status(500)
         .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+//get comments by id
+router.get("/:id/comments", (req, res) => {
+  Posts.findCommentById(req.params.id)
+    .then(comments => {
+      if (comments) {
+        res.status(200).json(comments);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
     });
 });
 
