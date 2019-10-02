d7e548066970b0a5cc97d4153bdc0f8368e016fd
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-02 09:22:55 -0700

    post displaying on get

--- api/postEndpoint.js
+++ api/postEndpoint.js
@@ -4,4 +4,18 @@ const db = require("../data/db");
 
 const router = express.Router();
 
+//Get Post
+
+router.get("/", (req, res) => {
+  db.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      });
+    });
+});
+
 module.exports = router;
